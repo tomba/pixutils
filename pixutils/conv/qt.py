92428f51a4d2a0d763cb31f213742d7ac82b068f
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-08-13 17:17:40 +0300

    pylint: hackfix PyQt6
    For some reason pylint is not able to import PyQt6, even if it's
    whitelisted.

--- pixutils/conv/qt.py
+++ pixutils/conv/qt.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (C) 2023, Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
 
+# XXX I have not been able to get PyQt6 imported to pylint
+# pylint: skip-file
+
 from PyQt6 import QtGui
 from .conv import data_to_rgb
 
