6707eaf45fce42bc5b5592e996eb0a610b4bf8ab
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-08-13 15:45:45 +0300

    Rename module to pixutils

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
 import unittest
-import iob
-import iob.fourcc_str
+import pixutils
+import pixutils.fourcc_str
 
 class TestDummy(unittest.TestCase):
     def test_dummy(self):
-        self.assertEqual(iob.PixelFormats.RGB888.name, 'RGB888')
+        self.assertEqual(pixutils.PixelFormats.RGB888.name, 'RGB888')
 
 class TestFourcc(unittest.TestCase):
     def test_fourcc(self):
-        self.assertEqual(iob.fourcc_str.str_to_fourcc('XR24'), 0x34325258)
-        self.assertEqual(iob.fourcc_str.fourcc_to_str(0x34325258), 'XR24')
+        self.assertEqual(pixutils.fourcc_str.str_to_fourcc('XR24'), 0x34325258)
+        self.assertEqual(pixutils.fourcc_str.fourcc_to_str(0x34325258), 'XR24')
         with self.assertRaises(ValueError):
-            iob.fourcc_str.str_to_fourcc('ABCDE')
+            pixutils.fourcc_str.str_to_fourcc('ABCDE')
         with self.assertRaises(ValueError):
-            iob.fourcc_str.str_to_fourcc('ABC')
+            pixutils.fourcc_str.str_to_fourcc('ABC')
 
 if __name__ == '__main__':
     unittest.main()
