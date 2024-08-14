c9dd9ea7ba89badf9aa18382b3987d6fcc6ef7b8
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-08-14 20:51:56 +0300

    tests: Add simple conv test

---
+++ tests/test_conv.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+import unittest
+import numpy as np
+
+from pixutils.conv.conv import to_rgb
+
+TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+
+class TestRGBConv(unittest.TestCase):
+    def test_rgb(self):
+        tests = [
+            ("BGR888", 2, 2, 0,
+             (1, 2, 3, 4, 5, 6,
+              1, 2, 3, 4, 5, 6),
+             (1, 2, 3, 4, 5, 6,
+              1, 2, 3, 4, 5, 6))
+            #("RGB888", 64, 48, 0, "rgb888-in.raw", "rgb888-out.raw"),
+        ]
+
+        for fmt, w, h, bytesperline, data_in, data_ref in tests:
+            if isinstance(data_in, str):
+                data_in = np.fromfile(TEST_PATH + '/' + data_in, dtype=np.uint8)
+            elif isinstance(data_in, (list, tuple)):
+                data_in = np.array(data_in, dtype=np.uint8)
+
+            if isinstance(data_ref, str):
+                data_ref = np.fromfile(TEST_PATH + '/' + data_ref, dtype=np.uint8)
+            elif isinstance(data_ref, (list, tuple)):
+                data_ref = np.array(data_ref, dtype=np.uint8)
+
+            data_out = to_rgb(fmt, w, h, bytesperline, data_in)
+
+            # Flatten for comparison
+            data_out = data_out.flatten()
+
+            self.assertTrue(np.array_equal(data_out, data_ref))
+
+if __name__ == '__main__':
+    unittest.main()
