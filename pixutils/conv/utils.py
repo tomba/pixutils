089ee7910902d20b6dde9c7d71defd9be25e0446
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2026-04-21 22:07:01 +0300

    conv: Add strip_padding() helper
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

---
+++ pixutils/conv/utils.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (C) 2023, Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
+
+from __future__ import annotations
+
+import numpy as np
+import numpy.typing as npt
+
+from pixutils.formats import PixelFormat
+
+
+# Note: the callers of this function should be fixed to handle
+# the stride properly
+def strip_padding(
+    data: npt.NDArray[np.uint8], height: int, strides: tuple[int, ...], fmt: PixelFormat, width: int
+) -> npt.NDArray[np.uint8]:
+    planes = []
+    offset = 0
+    for i, plane in enumerate(fmt.planes):
+        plane_h = height // plane.vsub
+        row_bytes = fmt.stride(width, i)
+        plane_data = data[offset : offset + strides[i] * plane_h]
+        if strides[i] != row_bytes:
+            plane_data = plane_data.reshape((plane_h, strides[i]))[:, :row_bytes].flatten()
+        planes.append(plane_data)
+        offset += strides[i] * plane_h
+    return np.concatenate(planes) if len(planes) > 1 else planes[0]
